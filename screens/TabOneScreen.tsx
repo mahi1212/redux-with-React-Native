dd799c6f8d68eae55694f092b9b54406de17b580
mahi1212 <mahi121.mr@gmail.com>
2022-11-04 20:50:28 +0600

    first commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,24 @@
-import { StyleSheet } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
+import { useDispatch } from 'react-redux';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { decrement, increment } from '../store/counterSlice';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const dispatch = useDispatch()
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={styles.buttons}>
+        <Pressable style={styles.signleButton} onPress = {()=> dispatch(increment())}>
+          <Text>Increment</Text>
+        </Pressable>
+        <Pressable style={styles.signleButton} onPress={() => {dispatch(decrement())}}>
+          <Text>Decrement</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -29,4 +38,14 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  buttons:{
+    flexDirection: 'row',
+    // flexFlow : 1
+    
+  },
+  signleButton :{
+    backgroundColor: 'purple',
+    padding: 20,
+    margin: 10
+  }
 });
